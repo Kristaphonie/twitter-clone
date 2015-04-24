6d6f06fa591b4c82ad6d0c1afd032ad8531f1f01
Kristaphonie <csaunders@getjive.com>
2015-04-23 19:03:00 -0600

    coolio

--- animations.js
+++ animations.js
@@ -1,11 +1,40 @@
-$('.tweet-compose').on('click', function () {
-	$('.tweet-compose').css({'width': '100%', 'height': '5em'});
-	$('#char-count').css({'visibility': 'visible'});
-	$('#tweet-submit').css({'visibility': 'visible'});
-});
+$(document).ready(function () {
+	$('.tweet-compose').on('click', function () {
+		$('.tweet-compose').css({'width': '100%', 'height': '5em'});
+		$('#char-count').css({'visibility': 'visible'});
+		$('#tweet-submit').css({'visibility': 'visible'});
+	});
+	
+	
+	$('.tweet-compose').on('keyup', function () {
+		var numTextLength = 140 - $('.tweet-compose').val().length;
 
-var textNum = $('#char-count');
-console.log(textNum);
-textNum.on('keydown', function () {
-	textNum.textContent() - 1;
-});
+		console.log(numTextLength);
+		$('#char-count').text(numTextLength);
+
+		if (numTextLength <= 10) {
+			$('#char-count').css({'color': 'red'});
+		} else {
+			$('#char-count').css({'color': 'grey'});
+		}
+
+		if (numTextLength <= 0) {
+			$('#char-count').css({'visibility': 'hidden'});
+			$('#tweet-submit').css({'visibility': 'hidden'});
+		} else {
+			$('#char-count').css({'visibility': 'visible'});
+			$('#tweet-submit').css({'visibility': 'visible'});
+		}
+	});
+
+	$('#tweet-submit').on('click', function () {
+		var tweetText = $('.tweet-compose').val();
+		var fullName = $('.account-name').text();
+		var image = $('avatar:first').text();
+		$('.tweet:first').clone().prependTo('#stream');
+		$('.tweet-text:first').text(tweetText);
+		$('.fullname:first').text(fullName);
+		console.log(image);
+		$('.avatar').attr('src', image);
+	})
+})
